b79719983e671feb311c2e497cb08250fbfef551
abderra-hmane <abderrahmanewinz@gmail.com>
2024-03-19 15:52:35 +0000

    added js function and modifier

--- snake.js
+++ snake.js
@@ -1,19 +1,26 @@
 let snake_lis = document.querySelectorAll(".snake-menu li");
 
-
 function removeActive(elements) {
     elements.forEach(e => {
-        
-        e.addEventListener("click",e=>{
-            
-            // remove the active class 
-        elements.forEach(element => {
-            element.classList.remove("active");
+        // Add event listener to each li element
+        e.addEventListener("click", e => {
+            // Remove active class from all li elements
+            elements.forEach(element => {
+                element.classList.remove("active");
+            });
+
+            // Add active class to the clicked li element
+            e.target.classList.add("active");
         });
 
-        e.target.classList.add("active");
-        })
-    
-    })
+        // Prevent the click event from bubbling up when clicking on <a> elements
+        let links = e.querySelectorAll('a');
+        links.forEach(link => {
+            link.addEventListener('click', e => {
+                e.stopPropagation(); // Stop event propagation
+            });
+        });
+    });
 }
+
 removeActive(snake_lis);
